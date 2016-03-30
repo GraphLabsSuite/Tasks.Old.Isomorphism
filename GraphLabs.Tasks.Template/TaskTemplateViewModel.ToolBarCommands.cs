bab9b0eec197a1dde97fba637389a97ab84a0e14
Илья Юдов <iyudov@d9d37ca5-d66a-4e4d-95ed-4e3758bb3d47>
2016-03-30 17:47:32 +0000

    git-svn-id: https://svtz.ru/svn/graphlabs.tasks.isomorphism@1 d9d37ca5-d66a-4e4d-95ed-4e3758bb3d47

---
+++ GraphLabs.Tasks.Template/TaskTemplateViewModel.ToolBarCommands.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Castle.Core.Internal;
+using GraphLabs.CommonUI.Controls.ViewModels;
+using GraphLabs.Graphs;
+
+namespace GraphLabs.Tasks.Template
+{
+    public partial class TaskTemplateViewModel
+    {
+        #region Полезности
+
+        private const string ImageResourcesPath = @"/GraphLabs.Tasks.Template;component/Images/";
+
+        private Uri GetImageUri(string imageFileName)
+        {
+            return new Uri(ImageResourcesPath + imageFileName, UriKind.Relative);
+        }
+
+        #endregion
+
+        private void InitToolBarCommands()
+        {
+            new HelpDialog(Strings.Strings_RU.stage3Help).Show();
+
+            #region Третий этап
+            #region Видимость
+
+            var vis = true;
+
+            var phase3VisCommand = new ToolBarInstantCommand(
+                () =>
+                {
+                    WorkspaceGraphVisibility = vis ? Visibility.Visible : Visibility.Collapsed;
+                    vis = !vis;
+                },
+                () => _state == State.Nothing
+                )
+            {
+                Description = "Скрыть / Показать слой зелёного графа",
+                Image = new BitmapImage(GetImageUri("Info.png"))
+            };
+            #endregion
+
+            #region Справка
+            var phase3HelpCommand = new ToolBarInstantCommand(
+                () => new HelpDialog(Strings.Strings_RU.stage3Help).Show(),
+                () => _state == State.Nothing
+                )
+            {
+                Description = Strings.Strings_RU.buttonHelp,
+                Image = new BitmapImage(GetImageUri("Info.png"))
+            };
+            #endregion
+
+            #region Завершить этап
+            var phase3Command = new ToolBarInstantCommand(
+                () =>
+                {
+                    if (WorkspaceGraph.Vertices[0].Name != "True")
+                    {
+                        UserActionsManager.RegisterMistake(Strings.Strings_RU.stage3Mistake1, 10);
+                    }
+                    else
+                    {
+                        UserActionsManager.RegisterInfo(Strings.Strings_RU.stage3Done);
+                    }
+                },
+                () => _state == State.Nothing
+                )
+            {
+                Image = new BitmapImage(GetImageUri("Check.png")),
+                Description = Strings.Strings_RU.stage3DoneButtonDisc
+            };
+            #endregion
+            #endregion
+            
+            Phase3ToolBarCommands = new ObservableCollection<ToolBarCommandBase>
+            {
+                phase3VisCommand,
+                phase3Command,
+                phase3HelpCommand
+            };
+        }
+    }
+}
---
+++ GraphLabs.Tasks.Template/TaskTemplateViewModel.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using GraphLabs.Common;
+using GraphLabs.Common.Utils;
+using GraphLabs.CommonUI;
+using GraphLabs.CommonUI.Controls.ViewModels;
+using GraphLabs.Graphs;
+using GraphLabs.Graphs.DataTransferObjects.Converters;
+using GraphLabs.Graphs.UIComponents.Visualization;
+
+namespace GraphLabs.Tasks.Template
+{
+    /// <summary> ViewModel для TaskTemplate </summary>
+    public partial class TaskTemplateViewModel : TaskViewModelBase<TaskTemplate>
+    {
+        /// <summary> Текущее состояние </summary>
+        private enum State
+        {
+            /// <summary> Пусто </summary>
+            Nothing,
+            /// <summary> Добавление рёбер </summary>
+            EdgesAdding
+        }
+
+        /// <summary> Текущее состояние </summary>
+        private State _state;
+
+        /// <summary> Допустимые версии генератора, с помощью которого сгенерирован вариант </summary>
+        private readonly Version[] _allowedGeneratorVersions = {  new Version(1, 0) };
+        
+        /// <summary> Допустимые версии генератора </summary>
+        protected override Version[] AllowedGeneratorVersions
+        {
+            get { return _allowedGeneratorVersions; }
+        }
+
+
+        #region Public свойства вьюмодели
+
+        /// <summary> Идёт загрузка данных? </summary>
+        public static readonly DependencyProperty IsLoadingDataProperty = DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.IsLoadingData), 
+            typeof(bool), 
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(false));
+
+        /// <summary> Разрешено перемещение вершин? </summary>
+        public static readonly DependencyProperty IsMouseVerticesMovingEnabledProperty = DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.IsMouseVerticesMovingEnabled),
+            typeof(bool),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(true));
+
+        /// <summary> Разрешено добавление рёбер? </summary>
+        public static readonly DependencyProperty IsEdgesAddingEnabledProperty = DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.IsEgesAddingEnabled),
+            typeof(bool),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(false));
+
+        /// <summary> Команды панели инструментов</summary>
+        public static readonly DependencyProperty Phase3ToolBarCommandsProperty = DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.Phase3ToolBarCommands),
+            typeof(ObservableCollection<ToolBarCommandBase>),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(default(ObservableCollection<ToolBarCommandBase>)));
+
+        /// <summary> Выданный в задании граф </summary>
+        public static readonly DependencyProperty GivenGraphProperty =
+            DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.GivenGraph),
+            typeof(IGraph),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(default(IGraph)));
+        
+        ///<summary> Фоновый граф для изоморфизма </summary>
+        public static readonly DependencyProperty BackgroundGraphProperty =
+            DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.BackgroundGraph),
+            typeof(IGraph),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(default(IGraph)));
+
+        ///<summary> Рабочий граф для изоморфизма </summary>
+        public static readonly DependencyProperty WorkspaceGraphProperty =
+            DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.WorkspaceGraph),
+            typeof(IGraph),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(default(IGraph)));
+
+        ///<summary> Видимость рабочего графа для изоморфизма </summary>
+        public static readonly DependencyProperty WorkspaceGraphVisibilityProperty =
+            DependencyProperty.Register(
+            ExpressionUtility.NameForMember((TaskTemplateViewModel m) => m.WorkspaceGraphVisibility),
+            typeof(Visibility),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(Visibility.Collapsed));
+
+        /// <summary> Радиус </summary>
+        public static readonly DependencyProperty DefaultVertexRadiusProperty =
+            DependencyProperty.Register(
+            ExpressionUtility.NameForMember((GraphVisualizer v) => v.DefaultVertexRadius),
+            typeof(double),
+            typeof(TaskTemplateViewModel),
+            new PropertyMetadata(default(double)));
+
+        /// <summary> Идёт загрузка данных? </summary>
+        public bool IsLoadingData
+        {
+            get { return (bool)GetValue(IsLoadingDataProperty); }
+            private set { SetValue(IsLoadingDataProperty, value); }
+        }
+
+        /// <summary> Разрешено перемещение вершин? </summary>
+        public bool IsMouseVerticesMovingEnabled
+        {
+            get { return (bool)GetValue(IsMouseVerticesMovingEnabledProperty); }
+            set { SetValue(IsMouseVerticesMovingEnabledProperty, value); }
+        }
+
+        /// <summary> Разрешено добавление рёбер? </summary>
+        public bool IsEgesAddingEnabled{
+            get { return (bool)GetValue(IsEdgesAddingEnabledProperty); }
+            set { SetValue(IsEdgesAddingEnabledProperty, value); }
+        }
+
+        /// <summary> Команды панели инструментов </summary>
+        public ObservableCollection<ToolBarCommandBase> Phase3ToolBarCommands
+        {
+            get { return (ObservableCollection<ToolBarCommandBase>)GetValue(Phase3ToolBarCommandsProperty); }
+            set { SetValue(Phase3ToolBarCommandsProperty, value); }
+        }
+
+        /// <summary> Выданные в задании графы </summary>
+        public IGraph GivenGraph
+        {
+            get { return (IGraph)GetValue(GivenGraphProperty); }
+            set { SetValue(GivenGraphProperty, value); }
+        }
+
+        /// <summary> Фоновый граф для изоморфизма </summary>
+        public IGraph BackgroundGraph
+        {
+            get { return (IGraph)GetValue(BackgroundGraphProperty); }
+            set { SetValue(BackgroundGraphProperty, value); }
+        }
+
+        /// <summary> Рабочий граф для изоморфизма </summary>
+        public IGraph WorkspaceGraph
+        {
+            get { return (IGraph)GetValue(WorkspaceGraphProperty); }
+            set { SetValue(WorkspaceGraphProperty, value); }
+        }
+
+        /// <summary> Видимость рабочего графа </summary>
+        public Visibility WorkspaceGraphVisibility
+        {
+            get { return (Visibility)GetValue(WorkspaceGraphVisibilityProperty); }
+            set { SetValue(WorkspaceGraphVisibilityProperty, value); }
+        }
+
+        #endregion
+
+
+        /// <summary> Инициализация </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            UserActionsManager.PropertyChanged += (sender, args) => HandlePropertyChanged(args);
+            VariantProvider.PropertyChanged += (sender, args) => HandlePropertyChanged(args);
+            InitToolBarCommands();
+            SubscribeToViewEvents();
+        }
+
+        private void SubscribeToViewEvents()
+        {
+            View.Loaded += (sender, args) => StartVariantDownload();
+        }
+
+        /// <summary> Начать загрузку варианта </summary>
+        public void StartVariantDownload()
+        {
+            VariantProvider.DownloadVariantAsync();
+        }
+
+        private void HandlePropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == ExpressionUtility.NameForMember((IUiBlockerAsyncProcessor p) => p.IsBusy))
+            {
+                // Нас могли дёрнуть из другого потока, поэтому доступ к UI - через Dispatcher.
+                Dispatcher.BeginInvoke(RecalculateIsLoadingData);
+            }
+        }
+
+        private void RecalculateIsLoadingData()
+        {
+            IsLoadingData = VariantProvider.IsBusy || UserActionsManager.IsBusy;
+        }
+
+        /// <summary> Задание загружено </summary>
+        /// <param name="e"></param>
+        protected override void OnTaskLoadingComlete(VariantDownloadedEventArgs e)
+        {
+            // Мы вызваны из другого потока. Поэтому работаем с UI-элементами через Dispatcher.
+            Dispatcher.BeginInvoke(() =>
+            {
+                GivenGraph = GraphSerializer.Deserialize(e.Data);
+                WorkspaceGraph = GivenGraph;
+                BackgroundGraph = GivenGraph;
+            });
+
+            //var number = e.Number; -- м.б. тоже где-то показать надо
+            //var version = e.Version;
+        }
+    }
+}
