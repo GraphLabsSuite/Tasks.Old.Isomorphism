76837e4385d815eb13b6da7d8b10ca4a22e0ab71
Илья Юдов <iyudov@d9d37ca5-d66a-4e4d-95ed-4e3758bb3d47>
2016-03-30 18:14:51 +0000

    git-svn-id: https://svtz.ru/svn/graphlabs.tasks.isomorphism@3 d9d37ca5-d66a-4e4d-95ed-4e3758bb3d47

---
+++ GraphLabs.Tasks.Template/AnswerDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GraphLabs.Tasks.Template
+{
+    public partial class AnswerDialog : ChildWindow
+    {
+        private readonly bool _inters;
+
+        public AnswerDialog(bool b)
+        {
+            InitializeComponent();
+
+            _inters = b;
+
+            label.Name = _inters ? "OK!" : "Совместите графы";
+
+            ans_positive.Checked += (s, a) =>
+            {
+                label.Name = _inters ? "OK!" : "Совместите графы";
+                textBox.IsReadOnly = true;
+            };
+            ans_negative.Checked += (s, a) =>
+            {
+                label.Name = "Опишите причину в поле ввода";
+                textBox.IsReadOnly = false;
+            };
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (
+                ans_positive.IsChecked.HasValue && ans_positive.IsChecked.Value && _inters ||
+                ans_negative.IsChecked.HasValue && ans_negative.IsChecked.Value && textBox.Text.Length != 0)
+            {
+                //TODO: Выдать текст ответа
+
+                DialogResult = true;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
+
